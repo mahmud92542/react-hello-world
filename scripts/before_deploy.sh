b9c0ae47cdfad142e1de7f264d7b216274b4accc
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2023-08-09 00:51:26 +0600

    Update before_deploy.sh

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Stop the PM2 process
-pm2 stop hello-world
-
-# Delete the PM2 process
-pm2 delete hello-world
+# Stop the PM2 process if it's running
+if pm2 describe hello-world &> /dev/null; then
+    pm2 stop hello-world
+    pm2 delete hello-world
+fi
 
 # Remove the directory contents
 rm -rf /home/ubuntu/react/react-hello-world/*
 
 # Navigate to the directory containing build.zip
 cd /home/ubuntu/react/react-hello-world
+
+# Check if the build.zip file exists before unzipping
+if [ -f build.zip ]; then
+    # Unzip the build.zip file
+    unzip build.zip
+fi

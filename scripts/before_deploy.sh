12506af55d10bae45ee0a26c0e789f2b7adcd07b
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2023-08-09 01:40:04 +0600

    Update before_deploy.sh

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Set the HOME environment variable for PM2
-export HOME=/etc
-
-# Stop and delete the PM2 process if it's running
-if pm2 describe hello-world &> /dev/null; then
-    pm2 stop hello-world
-    pm2 delete hello-world
-fi
-
 # Remove the directory contents
 rm -rf /home/ubuntu/react/react-hello-world/*

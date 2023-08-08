2015132f012cce3ec8f9ce0e8c18c1401b730992
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2023-08-09 01:43:36 +0600

    Update after_deploy.sh

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -2,7 +2,7 @@
 
 # Start the PM2 process for the React app
 cd /home/ubuntu/react/react-hello-world
-pm2 serve build 3000 --spa --name hello-world &
+pm2 restart hello-world &
 
 # Reload Nginx configuration
 sudo systemctl reload nginx
